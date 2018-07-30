13f6d59bf3b904abe963faa5bcf456bdfc280956
xiaobooo <363058661@qq.com>
2018-07-30 16:54:09 +0800

    update

---
+++ CODE/Learning_OpenGL/Light_Color/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Light_Color
+//
+//  Created by boone on 2018/7/30.
+//  Copyright © 2018年 boone. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
