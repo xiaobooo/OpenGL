8f380f1cb00c40bc38a973cd70fafc27a130085b
xiaobooo <363058661@qq.com>
2018-07-30 15:28:22 +0800

    UPDATE

---
+++ CODE/Learning_OpenGL/Learning_OpenGL/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Learning_OpenGL
+//
+//  Created by boone on 2018/7/30.
+//  Copyright © 2018年 boone. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
