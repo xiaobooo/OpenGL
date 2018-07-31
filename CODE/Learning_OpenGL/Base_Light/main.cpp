148dfd88b22feaa8610181eed87238b0f4cff943
xiaobooo <363058661@qq.com>
2018-07-31 13:50:11 +0800

    update

--- CODE/Learning_OpenGL/Base_Light/main.cpp
+++ CODE/Learning_OpenGL/Base_Light/main.cpp
@@ -180,6 +180,10 @@ int main()
         glClearColor(0.1f, 0.1f, 0.1f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
         
+        // change the light's position values over time (can be done anywhere in the render loop actually, but try to do it at least before using the light source positions)
+        lightPos.x = 1.0f + sin(glfwGetTime()) * 2.0f;
+        lightPos.y = sin(glfwGetTime() / 2.0f) * 1.0f;
+        
         // be sure to activate shader when setting uniforms/drawing objects
         lightingShader.use();
         lightingShader.setVec3("objectColor", 1.0f, 0.5f, 0.31f);
