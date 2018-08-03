eec0e4a80776ad136eadd008744472b9e554feba
xiaobooo <363058661@qq.com>
2018-08-03 15:37:32 +0800

    update

--- CODE/Learning_OpenGL/Bloom/7.bloom_final.fs
+++ CODE/Learning_OpenGL/Bloom/7.bloom_final.fs
@@ -9,15 +9,15 @@ uniform bool bloom;
 uniform float exposure;
 
 void main()
-{             
+{
     const float gamma = 2.2;
-    vec3 hdrColor = texture(scene, TexCoords).rgb;      
+    vec3 hdrColor = texture(scene, TexCoords).rgb;
     vec3 bloomColor = texture(bloomBlur, TexCoords).rgb;
     if(bloom)
         hdrColor += bloomColor; // additive blending
     // tone mapping
     vec3 result = vec3(1.0) - exp(-hdrColor * exposure);
-    // also gamma correct while we're at it       
+    // also gamma correct while we're at it
     result = pow(result, vec3(1.0 / gamma));
     FragColor = vec4(result, 1.0);
 }
