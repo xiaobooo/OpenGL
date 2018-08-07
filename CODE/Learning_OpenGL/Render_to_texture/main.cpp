f398b84fa967e3946fc6c9f1caca17b3440fb3bd
xiaobooo <363058661@qq.com>
2018-08-07 09:34:33 +0800

    update

--- CODE/Learning_OpenGL/Render_to_texture/main.cpp
+++ CODE/Learning_OpenGL/Render_to_texture/main.cpp
@@ -32,7 +32,7 @@ const unsigned int SCR_WIDTH = 1280;
 const unsigned int SCR_HEIGHT = 720;
 
 // camera
-Camera camera(glm::vec3(0.0f, 10.0f, 0.0f));
+Camera camera(glm::vec3(0.0f, 0.0f, 0.0f));
 float lastX = (float)SCR_WIDTH / 2.0;
 float lastY = (float)SCR_HEIGHT / 2.0;
 bool firstMouse = true;
