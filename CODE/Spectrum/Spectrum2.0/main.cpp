7211185cff1ebeeefc454faa58ea971a4a5bf472
xiaobooo <363058661@qq.com>
2018-08-07 10:00:31 +0800

    update

--- CODE/Spectrum/Spectrum2.0/main.cpp
+++ CODE/Spectrum/Spectrum2.0/main.cpp
@@ -155,7 +155,7 @@ int main()
         
         // 渲染
         // ------
-        glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
+        glClearColor(0.1f, 0.1f, 0.1f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT);
         
         // 频谱图绘制
