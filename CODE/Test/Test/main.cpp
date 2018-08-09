f2fd9010789d840818e8f69fcb7f0a395491fb61
xiaobooo <363058661@qq.com>
2018-08-09 15:13:03 +0800

    update

--- CODE/Test/Test/main.cpp
+++ CODE/Test/Test/main.cpp
@@ -19,6 +19,17 @@
 
 using namespace std;
 
+struct wav_struct
+{
+    unsigned long file_size;        //文件大小
+    unsigned short channel;            //通道数
+    unsigned long frequency;        //采样频率
+    unsigned long Bps;                //Byte率
+    unsigned short sample_num_bit;    //一个样本的位数
+    unsigned long data_size;        //数据大小
+    unsigned char *data;            //音频数据
+};
+
 vector<float> vertices;    //用于存储pcm文件解析出的数据
 int istart=0;
 int wstart=0;
