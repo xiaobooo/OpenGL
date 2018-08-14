d582d85d555bf2d8ef1099125134129843d8485e
xiaobooo <363058661@qq.com>
2018-08-14 16:44:26 +0800

    update

--- CODE/Spectrum/Spectrum4.0/main.cpp
+++ CODE/Spectrum/Spectrum4.0/main.cpp
@@ -133,34 +133,33 @@ int main()
     float* arr1 = new float[6*n];
     float* arr2 = new float[3*n];
     
+    int i=0;
     float xstart=-1.0;
-    int j=0;
+    
     //直线型频谱图数据存储
-    for(int i=0;i<n; )    //用迭代器的方式输出容器对象的值
+    for(vector<float>::iterator it = vertices.begin(); it != vertices.end(); it+=2 )    //用迭代器的方式输出容器对象的值
     {
-        float temp =sqrt(out[j][0]*out[j][0]+out[j][1]*out[j][1])/10000;
-        j++;
-        
-        arr[i++]=xstart;
+        arr[i++]=xstart;     //圆上的点
         arr[i++]=0.0f;
         arr[i++]=0.0f;
         
-        arr[i++]=xstart;
-        arr[i++]=temp;
+        arr[i++]=xstart;     //由圆向外延伸的终点，表示频谱
+        arr[i++]=*it;
         arr[i++]=0.0f;
         
         xstart=xstart+0.002;
         if (xstart>1.0) {
-            xstart=-1.0;
+            xstart=-1.0;     //循环存储N个圆形频谱
         }
+        
     }
     
     //离散点频谱图数据存储
     xstart=-1.0;
-    j=0;
+    int j=0;
     for(int i=0;i<n; )    //用迭代器的方式输出容器对象的值
     {
-        float temp =sqrt(out[j][0]*out[j][0]+out[j][1]*out[j][1])/30000;
+        float temp =sqrt(out[j][0]*out[j][0]+out[j][1]*out[j][1])/3000;
         j++;
         
         arr1[i++]=xstart;
@@ -179,11 +178,14 @@ int main()
     
     //波形频谱图数据存储
     xstart=-1.0;
-    int i=0;
-    for(vector<float>::iterator it = vertices.begin(); it != vertices.end(); it+=2 )    //用迭代器的方式输出容器对象的值
+    j=0;
+    for(int i=0;i<n; )    //用迭代器的方式输出容器对象的值
     {
+        float temp =sqrt(out[j][0]*out[j][0]+out[j][1]*out[j][1])/3000;
+        j++;
+        
         arr2[i++]=xstart;
-        arr2[i++]=-*it;
+        arr2[i++]=-temp;
         arr2[i++]=0.0f;
         
         xstart=xstart+0.001;
@@ -320,6 +322,7 @@ void drawLine()
             blueValue=blueValue+0.002;
         }
         
+        glLineWidth(8);
         glDrawArrays(GL_LINES, i, 2);
     }
     
