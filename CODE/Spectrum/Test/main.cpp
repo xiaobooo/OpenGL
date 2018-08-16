70bd559c4d22b433a5c91a3c7b523fbadf70e065
xiaobooo <363058661@qq.com>
2018-08-16 09:48:18 +0800

    update

--- CODE/Spectrum/Test/main.cpp
+++ CODE/Spectrum/Test/main.cpp
@@ -8,50 +8,35 @@
 
 #define OLD_FILE_PATH "/Users/boone/Desktop/Music/Seve.pcm"     //PCM源文件
 
-#include <glad/glad.h>
-#include <GLFW/glfw3.h>
-
-#include <glm/glm.hpp>
-#include <glm/gtc/matrix_transform.hpp>
-#include <glm/gtc/type_ptr.hpp>
-
-#include <learnopengl/shader_m.h>
-#include <learnopengl/camera.h>
-
 #include <iostream>
 #include <vector>
 #include <cmath>
 #include <unistd.h>
 
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+#include <fftw3.h>
+
+#include <learnopengl/shader.h>
+
 using namespace std;
 
+vector<float> vertices;    // 用于存储pcm文件解析出的数据
+int istart=0;
+int wstart=0;
+int pstart=0;
+int n;       //记录pcm文件中数据个数
+
 void framebuffer_size_callback(GLFWwindow* window, int width, int height);
-void mouse_callback(GLFWwindow* window, double xpos, double ypos);
-void scroll_callback(GLFWwindow* window, double xoffset, double yoffset);
 void processInput(GLFWwindow *window);
 void drawLine();
+void drawPoint();
+void drawWave();
+void drawTest();
 
 // settings
-const unsigned int SCR_WIDTH = 800;
-const unsigned int SCR_HEIGHT = 800;
-
-// camera
-Camera camera(glm::vec3(0.0f, 0.0f, 1.0f));
-float lastX = SCR_WIDTH / 2.0f;
-float lastY = SCR_HEIGHT / 2.0f;
-bool firstMouse = true;
-
-// timing
-float deltaTime = 0.0f;
-float lastFrame = 0.0f;
-
-// lighting
-glm::vec3 lightPos(0.0f, 0.0f, -0.1f);
-
-//draw
-vector<float> vertices;    //用于存储pcm文件解析出的数据
-int istart=0;
-int n;       //记录pcm文件中数据个数
+const unsigned int SCR_WIDTH = 1024;
+const unsigned int SCR_HEIGHT = 600;
 
 //PCM文件数据解码保存到数组中
 void fileOutput()
@@ -67,16 +52,16 @@ void fileOutput()
         if(size>0)
         {
             //-------------------------------------------------------------------------------------------------------------------------
-            if(pcm_In<0){
-                pcm_In=-pcm_In;
-            }
-            vertices.push_back((float)pcm_In/30000);
+            //            if(pcm_In<0){
+            //                pcm_In=-pcm_In;
+            //            }
+            vertices.push_back(pcm_In/(double)32768);
+            //  cout<<pcm_In<<"     ";
         }
         i++;
     }
     
-    n=i;
-    //    cout<<"数据个数： "<<n<<endl;
+    n=i*2;
     
     fclose(fp);
 }
@@ -107,13 +92,8 @@ int main()
     }
     glfwMakeContextCurrent(window);
     glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
-    glfwSetCursorPosCallback(window, mouse_callback);
-    glfwSetScrollCallback(window, scroll_callback);
     
-    // tell GLFW to capture our mouse
-    glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_DISABLED);
-    
-    // glad: 加载所有OpenGL函数指针
+    // glad: 家在所有OpenGL函数指针
     // ---------------------------------------
     if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
     {
@@ -121,247 +101,277 @@ int main()
         return -1;
     }
     
-    glEnable(GL_MULTISAMPLE); // Enabled by default on some drivers, but not all so always enable to make sure
-    glEnable (GL_LINE_SMOOTH);//启用线抗锯齿,边缘会降低其alpha值
-    glEnable (GL_BLEND);//启用混合
-    glEnable(GL_DEPTH_TEST);
     
     // 构建并编译着色器程序
     // ------------------------------------
-    Shader ourShader("/Users/boone/Desktop/Github/OpenGL/CODE/Spectrum/Test/spectrum.vs","/Users/boone/Desktop/Github/OpenGL/CODE/Spectrum/Test/spectrum.fs");
-    Shader lampShader("/Users/boone/Desktop/Github/OpenGL/CODE/Spectrum/Test/lamp.vs", "/Users/boone/Desktop/Github/OpenGL/CODE/Spectrum/Test/lamp.fs");
-    // 设置顶点数据（和缓冲区）并配置顶点属性
-    // ------------------------------------------------------------------
+    Shader ourShader("/Users/boone/Desktop/Github/OpenGL/CODE/Spectrum/Spectrum4.0/shader/spectrum.vs", "/Users/boone/Desktop/Github/OpenGL/CODE/Spectrum/Spectrum4.0/shader/spectrum.fs");
+    
+    Shader waveShader("/Users/boone/Desktop/Github/OpenGL/CODE/Spectrum/Spectrum4.0/shader/wave.vs","/Users/boone/Desktop/Github/OpenGL/CODE/Spectrum/Spectrum4.0/shader/wave.fs");
+    
+    Shader pointShader("/Users/boone/Desktop/Github/OpenGL/CODE/Spectrum/Spectrum4.0/shader/point.vs","/Users/boone/Desktop/Github/OpenGL/CODE/Spectrum/Spectrum4.0/shader/point.fs");
+    
+    //FFTW
+    fftw_complex *in,*out;
+    fftw_plan p;
+    in = (fftw_complex*)fftw_malloc(sizeof(fftw_complex)*n);
+    out = (fftw_complex*)fftw_malloc(sizeof(fftw_complex)*n);
+    if (in==NULL||out==NULL) {
+        cout<<"ERROR: Fail to memory allocation"<<endl;
+    }else{
+        int  i=0;
+        for(vector<float>::iterator it = vertices.begin(); it != vertices.end(); it+=2 ){
+            in[i][0]=*it;
+            in[i][1]=0.0;
+            i++;
+        }
+    }
+    p = fftw_plan_dft_1d(n, in, out, FFTW_BACKWARD, FFTW_ESTIMATE);
+    fftw_execute(p);
+    fftw_destroy_plan(p);
+    fftw_cleanup();
+    
     float* arr = new float[6*n];
+    float* arr1 = new float[6*n];
+    float* arr2 = new float[3*n];
     
-    int i=0;
     float xstart=-1.0;
-    
-    for(vector<float>::iterator it = vertices.begin(); it != vertices.end(); it+=2 )    //用迭代器的方式输出容器对象的值
-    {
-        arr[i++]=xstart;     //圆上的点
+    int j=1000;
+    int i=0;
+    //直线型频谱图数据存储
+    while(j<n){
+        float temp =sqrt(out[j][0]*out[j][0]+out[j][1]*out[j][1])/30000;
+        j++;
+        
+        arr[i++]=xstart;
         arr[i++]=0.0f;
         arr[i++]=0.0f;
         
-        arr[i++]=xstart;     //由圆向外延伸的终点，表示频谱
-        arr[i++]=*it;
+        arr[i++]=xstart;
+        arr[i++]=temp;
         arr[i++]=0.0f;
         
         xstart=xstart+0.002;
         if (xstart>1.0) {
-            xstart=-1.0;     //循环存储N个圆形频谱
+            xstart=-1.0;
+        }
+    }
+    
+    //离散点频谱图数据存储
+    xstart=-1.0;
+    j=10000;
+    i=0;
+    while(j<n)
+    {
+        float temp =sqrt(out[j][0]*out[j][0]+out[j][1]*out[j][1])/30000;
+        j++;
+        
+        arr1[i++]=xstart;
+        arr1[i++]=temp;
+        arr1[i++]=0.0f;
+        
+        arr1[i++]=xstart;
+        arr1[i++]=-temp;
+        arr1[i++]=0.0f;
+        
+        xstart=xstart+0.005;
+        if (xstart>1.0) {
+            xstart=-1.0;
+        }
+    }
+    
+    //波形频谱图数据存储
+    xstart=-1.0;
+    i=0;
+    for(vector<float>::iterator it = vertices.begin(); it != vertices.end(); it+=2 )    //用迭代器的方式输出容器对象的值
+    {
+        if (*it<0) {
+            *it=-*it;
+        }
+        
+        arr2[i++]=xstart;
+        arr2[i++]=-*it;
+        arr2[i++]=0.0f;
+        
+        xstart=xstart+0.001;
+        if (xstart>1.0) {
+            xstart=-1.0;
         }
         
     }
     
-    float lamp_vertices[] = {
-        -0.5f, -0.5f, -0.5f,
-        0.5f, -0.5f, -0.5f,
-        0.5f,  0.5f, -0.5f,
-        0.5f,  0.5f, -0.5f,
-        -0.5f,  0.5f, -0.5f,
-        -0.5f, -0.5f, -0.5f,
-        
-        -0.5f, -0.5f,  0.5f,
-        0.5f, -0.5f,  0.5f,
-        0.5f,  0.5f,  0.5f,
-        0.5f,  0.5f,  0.5f,
-        -0.5f,  0.5f,  0.5f,
-        -0.5f, -0.5f,  0.5f,
-        
-        -0.5f,  0.5f,  0.5f,
-        -0.5f,  0.5f, -0.5f,
-        -0.5f, -0.5f, -0.5f,
-        -0.5f, -0.5f, -0.5f,
-        -0.5f, -0.5f,  0.5f,
-        -0.5f,  0.5f,  0.5f,
-        
-        0.5f,  0.5f,  0.5f,
-        0.5f,  0.5f, -0.5f,
-        0.5f, -0.5f, -0.5f,
-        0.5f, -0.5f, -0.5f,
-        0.5f, -0.5f,  0.5f,
-        0.5f,  0.5f,  0.5f,
-        
-        -0.5f, -0.5f, -0.5f,
-        0.5f, -0.5f, -0.5f,
-        0.5f, -0.5f,  0.5f,
-        0.5f, -0.5f,  0.5f,
-        -0.5f, -0.5f,  0.5f,
-        -0.5f, -0.5f, -0.5f,
-        
-        -0.5f,  0.5f, -0.5f,
-        0.5f,  0.5f, -0.5f,
-        0.5f,  0.5f,  0.5f,
-        0.5f,  0.5f,  0.5f,
-        -0.5f,  0.5f,  0.5f,
-        -0.5f,  0.5f, -0.5f,
-    };
-    
+    if (in!=NULL) {
+        fftw_free(in);
+    }
+    if (out!=NULL) {
+        fftw_free(out);
+    }
+    //直线型
     unsigned int VBO, VAO;
     glGenVertexArrays(1, &VAO);
     glGenBuffers(1, &VBO);
     
+    glBindVertexArray(VAO);
     glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    
     glBufferData(GL_ARRAY_BUFFER, 24*n, arr, GL_STATIC_DRAW);
     
-    glBindVertexArray(VAO);
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+    
+    glEnableVertexAttribArray(0);
+    
+    //离散型
+    unsigned int pointVBO, pointVAO;
+    glGenVertexArrays(1, &pointVAO);
+    glGenBuffers(1, &pointVBO);
+    
+    glBindVertexArray(pointVAO);
+    glBindBuffer(GL_ARRAY_BUFFER, pointVBO);
+    
+    glBufferData(GL_ARRAY_BUFFER, 24*n, arr1, GL_STATIC_DRAW);
     
     glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+    
     glEnableVertexAttribArray(0);
     
     
-    // first, configure the cube's VAO (and VBO)
-    unsigned int lightVBO, lightVAO;
-    glGenVertexArrays(1, &lightVAO);
-    glGenBuffers(1, &lightVBO);
+    //波形
+    unsigned int waveVAO,waveVBO;
+    glGenVertexArrays(1, &waveVAO);
+    glGenBuffers(1, &waveVBO);
     
-    glBindBuffer(GL_ARRAY_BUFFER, lightVBO);
-    glBufferData(GL_ARRAY_BUFFER, sizeof(lamp_vertices), lamp_vertices, GL_STATIC_DRAW);
+    glBindVertexArray(waveVAO);
+    glBindBuffer(GL_ARRAY_BUFFER, waveVBO);
     
-    glBindVertexArray(lightVAO);
+    glBufferData(GL_ARRAY_BUFFER, 12*n, arr2, GL_STATIC_DRAW);
     
-    // position attribute
     glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+    
     glEnableVertexAttribArray(0);
+    
     
     // 循环渲染
-    // -----------
     while (!glfwWindowShouldClose(window))
     {
-        // per-frame time logic
-        // --------------------
-        float currentFrame = glfwGetTime();
-        deltaTime = currentFrame - lastFrame;
-        lastFrame = currentFrame;
-        
         processInput(window);
         
         // 渲染
         // ------
-        glClearColor(0.1f, 0.1f, 0.1f, 1.0f);
-        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+        glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT);
         
         ourShader.use();
-        ourShader.setVec3("objectColor", 1.0f, 0.0f, 0.0f);
-        ourShader.setVec3("lightColor",  1.0f, 1.0f, 1.0f);
-        
-        // view/projection transformations
-        glm::mat4 projection = glm::perspective(glm::radians(camera.Zoom), (float)SCR_WIDTH / (float)SCR_HEIGHT, 0.1f, 100.0f);
-        glm::mat4 view = camera.GetViewMatrix();
-        ourShader.setMat4("projection", projection);
-        ourShader.setMat4("view", view);
-        
-        // world transformation
-        glm::mat4 model;
-        ourShader.setMat4("model", model);
         
         glBindVertexArray(VAO); // 激活VAO表示的顶点缓存
         if (istart<6*n) {   //到达终点之前每次绘制一帧的频谱图
             drawLine();
+            drawTest();
         }
         
-        // also draw the lamp object
-        lampShader.use();
-        lampShader.setMat4("projection", projection);
-        lampShader.setMat4("view", view);
-        model = glm::mat4();
-        model = glm::translate(model, lightPos);
-        model = glm::scale(model, glm::vec3(0.2f)); // a smaller cube
-        lampShader.setMat4("model", model);
+        pointShader.use();
+        glBindVertexArray(pointVAO); // 激活VAO表示的顶点缓存
+        if (pstart<6*n) {   //到达终点之前每次绘制一帧的频谱图
+            drawPoint();
+        }
         
-        glBindVertexArray(lightVAO);
-        glDrawArrays(GL_TRIANGLES, 0, 36);
-        
+        waveShader.use();
+        glBindVertexArray(waveVAO); // 激活VAO表示的顶点缓存
+        if (wstart<3*n) {   //到达终点之前每次绘制一帧的频谱图
+            drawWave();
+        }
         
         glfwSwapBuffers(window);
         glfwPollEvents();
     }
     
-    // 绘制完成后释放资源
-    // ------------------------------------------------------------------------
     glDeleteVertexArrays(1, &VAO);
-    glDeleteVertexArrays(1, &lightVAO);
     glDeleteBuffers(1, &VBO);
-    glDeleteBuffers(1, &lightVBO);
     
-    // glfw: 清除先前分配的所有GLFW资源
-    // ------------------------------------------------------------------
+    glDeleteVertexArrays(1, &waveVAO);
+    glDeleteBuffers(1, &waveVBO);
+    
     glfwTerminate();
     return 0;
 }
-
-// glfw:对相应的按键作出相应的响应
 
 void processInput(GLFWwindow *window)
 {
     if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
         glfwSetWindowShouldClose(window, true);
-    
-    if (glfwGetKey(window, GLFW_KEY_W) == GLFW_PRESS)
-        camera.ProcessKeyboard(FORWARD, deltaTime);
-    if (glfwGetKey(window, GLFW_KEY_S) == GLFW_PRESS)
-        camera.ProcessKeyboard(BACKWARD, deltaTime);
-    if (glfwGetKey(window, GLFW_KEY_A) == GLFW_PRESS)
-        camera.ProcessKeyboard(LEFT, deltaTime);
-    if (glfwGetKey(window, GLFW_KEY_D) == GLFW_PRESS)
-        camera.ProcessKeyboard(RIGHT, deltaTime);
 }
+
 // glfw: 每当窗口大小改变时，调用该回调函数
-// ---------------------------------------------------------------------------------------------
+// -------------------------------------------------------------------------------------
 void framebuffer_size_callback(GLFWwindow* window, int width, int height)
 {
-    //确保视口与新窗口尺寸匹配；注意宽度和
-    //高度将显著大于视网膜显示器上指定的高度。
     glViewport(0, 0, width, height);
 }
-void mouse_callback(GLFWwindow* window, double xpos, double ypos)
-{
-    if (firstMouse)
-    {
-        lastX = xpos;
-        lastY = ypos;
-        firstMouse = false;
-    }
-    
-    float xoffset = xpos - lastX;
-    float yoffset = lastY - ypos; // reversed since y-coordinates go from bottom to top
-    
-    lastX = xpos;
-    lastY = ypos;
-    
-    camera.ProcessMouseMovement(xoffset, yoffset);
-}
-
-// glfw: whenever the mouse scroll wheel scrolls, this callback is called
-// ----------------------------------------------------------------------
-void scroll_callback(GLFWwindow* window, double xoffset, double yoffset)
-{
-    camera.ProcessMouseScroll(yoffset);
-}
-
 //绘制频谱
 void drawLine()
 {
-    usleep(99900);   //通过延时实现频谱的显示频率
-    
     //颜色随机设置
     float redValue = 0.0f;
-    float yellowValue = 1.0f;
+    float blueValue = 1.0f;
     
     for (int i=istart; i<2000+istart; i+=2) {
-        glUniform4f(0, redValue, 1.0f, yellowValue, 1.0f);
+        
+        glUniform4f(0, redValue, 1.0f, blueValue, 1.0f);
         
         if (i<=1000+istart) {
             redValue=redValue+0.002;
-            yellowValue=yellowValue-0.002;
+            blueValue=blueValue-0.002;
         }else{
             redValue=redValue-0.002;
-            yellowValue=yellowValue+0.002;
+            blueValue=blueValue+0.002;
         }
         
         glDrawArrays(GL_LINES, i, 2);
     }
     
     istart+=2000;
+}
+
+//绘制离散型频谱
+void drawPoint()
+{
+    
+    for (int i=pstart; i<800+pstart; i++) {
+        
+        glUniform4f(0, 0.2f, 0.7f, 1.0f, 1.0f);
+        
+        glPointSize(3);
+        glDrawArrays(GL_POINTS, i, 1);
+    }
+    
+    pstart+=800;
+}
+//绘制波形频谱
+void drawWave()
+{
+    //颜色随机设置
+    float redValue = 0.0f;
+    float blueValue = 1.0f;
+    
+    for (int i=wstart; i<2000+wstart; i++) {
+        
+        glUniform4f(0, redValue, 1.0f ,blueValue, 1.0f);
+        
+        if (i<=500+wstart) {
+            redValue=redValue+0.002;
+            blueValue=blueValue-0.002;
+        }else{
+            redValue=redValue-0.002;
+            blueValue=blueValue+0.002;
+        }
+        
+        glPointSize(4);
+        glDrawArrays(GL_POINTS, i, 1);
+    }
+    
+    wstart+=2000;
+}
+
+//测试
+void drawTest()
+{
+    glDrawArrays(GL_LINES,1000,2);
 }
