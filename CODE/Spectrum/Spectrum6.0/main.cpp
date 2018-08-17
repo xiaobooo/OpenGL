41d5b7ca43e6a5c760e17858b2df0e329baafb51
xiaobooo <363058661@qq.com>
2018-08-17 09:28:00 +0800

    update

--- CODE/Spectrum/Spectrum6.0/main.cpp
+++ CODE/Spectrum/Spectrum6.0/main.cpp
@@ -6,10 +6,3 @@
 //  Copyright © 2018年 boone. All rights reserved.
 //
 
-#include <iostream>
-
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
-    return 0;
-}
