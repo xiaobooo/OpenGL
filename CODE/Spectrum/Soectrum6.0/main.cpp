6a5a570fa7deb3e86626901cf013c250bd6d82f7
刘渊博 <lyb4@meitu.com>
2018-08-20 13:35:47 +0800

    update

---
+++ CODE/Spectrum/Soectrum6.0/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Soectrum6.0
+//
+//  Created by boone on 2018/8/20.
+//  Copyright © 2018年 boone. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
