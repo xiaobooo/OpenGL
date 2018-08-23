7f5e1ef5d785d3ab6e0ecfad180a068c1c7f9914
刘渊博 <363058661@qq.com>
2018-08-23 09:49:39 +0800

    update

--- CODE/Learning_OpenGL/Render_to_texture/5.1.framebuffers_screen.fs
+++ CODE/Learning_OpenGL/Render_to_texture/5.1.framebuffers_screen.fs
@@ -5,36 +5,35 @@ in vec2 TexCoords;
 
 uniform sampler2D screenTexture;
 
-const float offset = 1.0 / 300.0;
-
 void main()
 {
-    vec2 offsets[9] = vec2[](
-                             vec2(-offset,  offset), // 左上
-                             vec2( 0.0f,    offset), // 正上
-                             vec2( offset,  offset), // 右上
-                             vec2(-offset,  0.0f),   // 左
-                             vec2( 0.0f,    0.0f),   // 中
-                             vec2( offset,  0.0f),   // 右
-                             vec2(-offset, -offset), // 左下
-                             vec2( 0.0f,   -offset), // 正下
-                             vec2( offset, -offset)  // 右下
-                             );
+    vec4 color=vec4(0.0);
+    int coreSize=3;
+    float texelOffset=1/150.0;
+    float kernel[9];
     
-    float kernel[9] = float[](
-                              1.0 / 16, 2.0 / 16, 1.0 / 16,
-                              2.0 / 16, 4.0 / 16, 2.0 / 16,
-                              1.0 / 16, 2.0 / 16, 1.0 / 16
-                              );
+    kernel[6]=1;kernel[7]=2;kernel[8]=1;
+    kernel[3]=2;kernel[4]=4;kernel[5]=2;
+    kernel[0]=1;kernel[1]=2;kernel[2]=1;
     
-    vec3 sampleTex[9];
-    for(int i = 0; i < 9; i++)
+    //移动高斯核,对原图做卷积计算
+    int index=0;
+    for(int y=0;y<coreSize;y++)
     {
-        sampleTex[i] = vec3(texture(screenTexture, TexCoords.st + offsets[i]));
+        for(int x=0;x<coreSize;x++)
+        {
+            
+            //原图像素点
+            vec4 currentColor=texture2D(screenTexture,TexCoords+vec2((-1+x)*texelOffset,(-1+y)*texelOffset));
+            //卷积计算
+            color+=currentColor*kernel[index++];
+        }
     }
-    vec3 col = vec3(0.0);
-    for(int i = 0; i < 9; i++)
-        col += sampleTex[i] * kernel[i];
+    //根据邻域内像素的加权平均灰度值去替代模板中心像素点的值
+    color/=16.0;
+    FragColor = color;
     
-    FragColor = vec4(col, 1.0);
+  //  vec3 col = texture(screenTexture, TexCoords).rgb;
+    
+  //  FragColor = vec4(col, 1.0);
 }
