f02bf8625668a33c9f1215b2ef5761f1a583bd2e
刘渊博 <363058661@qq.com>
2018-08-23 10:35:53 +0800

    update

--- CODE/Spectrum/Spectrum6.0/spectrum.fs
+++ CODE/Spectrum/Spectrum6.0/spectrum.fs
@@ -107,9 +107,9 @@ void main()
     vec3 f_color = hsvtorgb(hsv);
     f_color = ((f_color - 0.5) * max(u_contrast+1.0, 0.0)) + 0.5;
     
-   // float average = 0.2126 * f_color.x + 0.7152 * f_color.y + 0.0722 * f_color.z;
+    float average = 0.8*f_color.r+0.07*f_color.g+0.09*f_color.b;
     
-    FragColor = vec4(f_color.r, f_color.g, f_color.b, pixColor.a);
+    FragColor = vec4(average, average, average, pixColor.a);
     
     //FragColor = ourColor*vec4(1.0,1.0,1.0,0.5);
 }
