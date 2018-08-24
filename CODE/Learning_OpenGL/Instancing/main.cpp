56a5acf491a54693ccd80246d4f11be0267b5ff9
刘渊博 <363058661@qq.com>
2018-08-24 19:38:05 +0800

    update

--- CODE/Learning_OpenGL/Instancing/main.cpp
+++ CODE/Learning_OpenGL/Instancing/main.cpp
@@ -237,7 +237,6 @@ void processInput(GLFWwindow *window)
 void framebuffer_size_callback(GLFWwindow* window, int width, int height)
 {
     // make sure the viewport matches the new window dimensions; note that width and
-    // height will be significantly larger than specified on retina displays.
     glViewport(0, 0, width, height);
 }
 
