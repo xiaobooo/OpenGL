777e0a67ba29bdf846a28ee76f3c1e0fd02200ad
刘渊博 <363058661@qq.com>
2018-08-24 19:41:36 +0800

    update

--- CODE/Learning_OpenGL/Instancing/main.cpp
+++ CODE/Learning_OpenGL/Instancing/main.cpp
@@ -170,8 +170,6 @@ int main()
         
         processInput(window);
         
-        // render
-        // ------
         glClearColor(0.1f, 0.1f, 0.1f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
         
