91ed2707d40a89be43ed0d4ef18a49b9d8542a9e
刘渊博 <363058661@qq.com>
2018-08-27 15:46:45 +0800

    update

--- CODE/Learning_OpenGL/SkyBox/6.1.cubemaps.fs
+++ CODE/Learning_OpenGL/SkyBox/6.1.cubemaps.fs
@@ -9,8 +9,8 @@ uniform samplerCube skybox;
 
 void main()
 {
+    float ratio = 1.00 / 1.52;
     vec3 I = normalize(Position - cameraPos);
-    vec3 R = reflect(I, normalize(Normal));
+    vec3 R = refract(I, normalize(Normal), ratio);
     FragColor = vec4(texture(skybox, R).rgb, 1.0);
 }
-
