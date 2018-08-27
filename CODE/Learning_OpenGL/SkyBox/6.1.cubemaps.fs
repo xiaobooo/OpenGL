f43500c664fcf9c0d64150aa2f7e54e1ed2ac35f
刘渊博 <363058661@qq.com>
2018-08-27 15:21:02 +0800

    update

--- CODE/Learning_OpenGL/SkyBox/6.1.cubemaps.fs
+++ CODE/Learning_OpenGL/SkyBox/6.1.cubemaps.fs
@@ -1,11 +1,16 @@
 #version 330 core
 out vec4 FragColor;
 
-in vec2 TexCoords;
+in vec3 Normal;
+in vec3 Position;
 
-uniform sampler2D texture1;
+uniform vec3 cameraPos;
+uniform samplerCube skybox;
 
 void main()
-{    
-    FragColor = texture(texture1, TexCoords);
+{
+    vec3 I = normalize(Position - cameraPos);
+    vec3 R = reflect(I, normalize(Normal));
+    FragColor = vec4(texture(skybox, R).rgb, 1.0);
 }
+
