d5aa4fd33347162dcf88e602513fe7bb9a5cc323
刘渊博 <363058661@qq.com>
2018-08-29 10:44:48 +0800

    update

--- CODE/Learning_OpenGL/GLSL/main.cpp
+++ CODE/Learning_OpenGL/GLSL/main.cpp
@@ -43,6 +43,7 @@ int main()
 {
     // glfw: initialize and configure
     // ------------------------------
+    
     glfwInit();
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
