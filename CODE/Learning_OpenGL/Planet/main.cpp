5cd2328110b36c3d63b4b4df3d6bc2c1141007d0
刘渊博 <363058661@qq.com>
2018-08-30 15:05:55 +0800

    update

---
+++ CODE/Learning_OpenGL/Planet/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Planet
+//
+//  Created by boone on 2018/8/30.
+//  Copyright © 2018年 boone. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
