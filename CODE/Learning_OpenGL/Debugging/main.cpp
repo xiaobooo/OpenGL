fadf9a646f1b7ca80b8f432bacc42f14ec3a6ed1
刘渊博 <363058661@qq.com>
2018-08-31 13:58:36 +0800

    update

--- CODE/Learning_OpenGL/Debugging/main.cpp
+++ CODE/Learning_OpenGL/Debugging/main.cpp
@@ -241,10 +241,7 @@ int main()
     // -----------
     while (!glfwWindowShouldClose(window))
     {
-        // input
-        // -----
         processInput(window);
-        
         
         glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
